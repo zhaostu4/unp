cfdbecb3c4836b7f1d57197f76195cd961e2005c
hzq <1593409937@qq.com>
2018-06-10 20:03:09 +0800

    name and address

--- 10_sctp/sctpserv01.c
+++ 10_sctp/sctpserv01.c
@@ -1,4 +1,6 @@
-#include	"unp.h"
+#include	"../07_hzq/all_head.h"
+#include    "/usr/include/netinet/sctp.h"
+//#include    "string.h"
 int main(int argc, char **argv)
 {
 	int sock_fd,msg_flags;
@@ -14,35 +16,35 @@ int main(int argc, char **argv)
 		stream_increment = atoi(argv[1]);    /*1.默认情况下服务器响应所用的流号是在其接收消息的流号上加1
                                                                     该参数决定是否增长外来流号*/
                                                                    /* 创建一个SCTP一到多流式套接字 */
-        sock_fd = Socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
+        sock_fd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+	bzero(&servaddr, sizeof(servaddr));      /* 网际套接字清零 */
+	servaddr.sin_family = AF_INET;           /* 2.需要使用通配地址：将在任何关联中使用所有可用的本地地址 */
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(SERV_PORT);
 
-	Bind(sock_fd, (SA *) &servaddr, sizeof(servaddr));
+	bind(sock_fd, (SA *) &servaddr, sizeof(servaddr));
 	
-	bzero(&evnts, sizeof(evnts));
-	evnts.sctp_data_io_event = 1;
-	Setsockopt(sock_fd, IPPROTO_SCTP, SCTP_EVENTS,
+	bzero(&evnts, sizeof(evnts));                /* 3.修改套接字通知预定 */
+	evnts.sctp_data_io_event = 1;               /* 虽然默认开启，但是显式开启好一点 */
+	setsockopt(sock_fd, IPPROTO_SCTP, SCTP_EVENTS,
 		   &evnts, sizeof(evnts));
 
-	Listen(sock_fd, LISTENQ);
+	listen(sock_fd, LISTENQ);                  /* 4.开启外来关联 */
 	for ( ; ; ) {
-		len = sizeof(struct sockaddr_in);
-		rd_sz = Sctp_recvmsg(sock_fd, readbuf, sizeof(readbuf),
+		len = sizeof(struct sockaddr_in);     /* 5.服务器将阻塞在等待来自任何一个远程对端的消息 */
+		rd_sz = sctp_recvmsg(sock_fd, readbuf, sizeof(readbuf),
 			     (SA *)&cliaddr, &len,
 			     &sri,&msg_flags);
-		if(stream_increment) {
-			sri.sinfo_stream++;
+		if(stream_increment) {                    /* 检查流号 */
+			sri.sinfo_stream++;                      /* 6.sctp_get_no_strms()：获取最大流号 */
 			if(sri.sinfo_stream >= sctp_get_no_strms(sock_fd,(SA *)&cliaddr, len)) 
 				sri.sinfo_stream = 0;
 		}
-		Sctp_sendmsg(sock_fd, readbuf, rd_sz, 
+		sctp_sendmsg(sock_fd, readbuf, rd_sz, 
 			     (SA *)&cliaddr, len,
-			     sri.sinfo_ppid,
+			     sri.sinfo_ppid,                        /* 7.指定将随数据块传递的净荷协议标识ID(来自sir结构) */
 			     sri.sinfo_flags,
-			     sri.sinfo_stream,
-			     0, 0);
+			     sri.sinfo_stream,                    /* 8.timetolive=0,标识无限长生命周期 */
+			     0, 0);                                      /* 9.context=0,表禁止上下文 */
 	}
 }
--- 11.names/hostent.c
+++ 11.names/hostent.c
@@ -1,5 +1,5 @@
 #include	"unp.h"
-
+//#include	"../07_hzq/all_head.h"
 int
 main(int argc, char **argv)
 {
@@ -7,14 +7,14 @@ main(int argc, char **argv)
 	char			str[INET_ADDRSTRLEN];
 	struct hostent	*hptr;
 
-	while (--argc > 0) {
+	while (--argc > 0) {   //给每个命令行参数调用gethostbyname
 		ptr = *++argv;
 		if ( (hptr = gethostbyname(ptr)) == NULL) {
 			err_msg("gethostbyname error for host: %s: %s",
 					ptr, hstrerror(h_errno));
 			continue;
 		}
-		printf("official hostname: %s\n", hptr->h_name);
+		printf("official hostname: %s\n", hptr->h_name);   //输出规范主机名
 
 		for (pptr = hptr->h_aliases; *pptr != NULL; pptr++)
 			printf("\talias: %s\n", *pptr);
